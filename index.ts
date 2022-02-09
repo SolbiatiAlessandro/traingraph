d8efc0572427de4f5f622a45cb00674561e84fef
Alessandro Solbiati <lessandro@fb.com>
2022-02-09 22:53:51 +0000

    first commit

---
+++ index.ts
@@ -0,0 +1,21 @@
+import Graph from 'graphology';
+import Sigma from 'sigma';
+
+const container = document.getElementById("sigma-container") as HTMLElement;
+
+const graph = new Graph();
+
+
+graph.addNode("John", { x: 0, y: 10, size: 5, label: "John", color: "blue" });
+graph.addNode("Mary", { x: 10, y: 0, size: 3, label: "Mary", color: "red" });
+
+graph.addEdge('John', 'Mary');
+
+console.log(graph.order); //nodes
+console.log(graph.size); //edges
+
+graph.forEachNode(node => {
+	console.log(node);
+})
+
+const renderer = new Sigma(graph, container);
